daa4800c6889b4d0855cc2941c3cdbd736013782
Luke Lee <luke.lee.python@gmail.com>
2025-06-09 15:37:57 -0500

    added new functionality of fetching all options data, added decorators, other minor edits

--- code/support_functions.py
+++ code/support_functions.py
@@ -1,18 +1,47 @@
 import pandas as pd
 import numpy as np
 
-days_in_year = 365
+DAYS_IN_YEAR = 365
 period_dic = {
     '1d': 1,
     '5d': 5,
-    '1mo': round(days_in_year / 12),
-    '3mo': round(3 * days_in_year / 12),
-    '6mo': round(6 * days_in_year / 12),
-    '1y': days_in_year,
-    '2y': 2 * days_in_year,
-    '5y': 5 * days_in_year,
-    '10y': 10 * days_in_year
+    '1mo': round(DAYS_IN_YEAR / 12),
+    '3mo': round(3 * DAYS_IN_YEAR / 12),
+    '6mo': round(6 * DAYS_IN_YEAR / 12),
+    '1y': DAYS_IN_YEAR,
+    '2y': 2 * DAYS_IN_YEAR,
+    '5y': 5 * DAYS_IN_YEAR,
+    '10y': 10 * DAYS_IN_YEAR
 }
+IVOL_FLOOR = np.float64(1e-4)
+
+
+def clean_data(function):
+    """ Decorator used to clean data, only applied when the function rertuns a DataFrame"""
+    def filter_data(data_frame):
+        # remove un-wanted data
+        data_frame = data_frame.loc[data_frame.openInterest != 0, :]
+        data_frame = data_frame.loc[data_frame.ask > data_frame.bid, :]
+        data_frame = data_frame.loc[data_frame.impliedVolatility > IVOL_FLOOR, :]
+        cols_to_sort = ['strike']
+        if 'Expiration' in data_frame.columns:
+            data_frame.loc[:, 'dtox'] = (
+                    data_frame.loc[:, 'Expiration'].map(lambda x: pd.to_datetime(x).date())
+                    - data_frame.loc[:, 'lastTradeDate'].map(lambda x: x.date())
+            ).map(lambda x: x.days)
+            print('decorator')
+            data_frame = data_frame.loc[data_frame.dtox > 0, :]
+            cols_to_sort.append('Expiration')
+        data_frame = data_frame.sort_values(cols_to_sort).reset_index(drop=True)
+        return data_frame
+
+    def wrapper(*args, **kwargs):
+        result = function(*args, **kwargs)
+        if isinstance(result, pd.DataFrame):
+            result = filter_data(result)
+        return result
+
+    return wrapper
 
 
 def select_expiration(period_str, options, last_date):
@@ -55,6 +84,7 @@ def get_option_chain_tenor(period_str, ticker, last_date, underlying_last_close,
     Returns:
         options_selected: Selected Options data.
     """
+    # TODO: refactor using get_option_chain
     exp_selected, dtox_selected = select_expiration(period_str, ticker.options, last_date)
     print(ticker.info['longName'])
     option_chain_1m = ticker.option_chain(date=exp_selected)
@@ -119,7 +149,7 @@ def get_option_chain_strikes(ticker, last_date):
         call_data (pd.DataFrame): DataFrame of Call data
         put_data (pd.DataFrame):  DataFrame of Put data
     """
-
+    # TODO: refactor using get_option_chain
     strike_dict = get_strikes(ticker, last_date)
     strikes = list(strike_dict.values())
 
@@ -149,3 +179,44 @@ def get_option_chain_strikes(ticker, last_date):
     put_data.loc[:, 'dtox'] = list(map(lambda x: (pd.to_datetime(x).date() - last_date.to_pydatetime().date()).days,
                                        put_data.loc[:, 'Expiration']))
     return call_data, put_data
+
+
+@clean_data
+def get_option_chain(ticker, strikes_selected=None, tenors_selected=None):
+    """
+    For all the available tenors, fetch the data with the tenors that are ITM, ATM, and OTM.
+
+    Args:
+        ticker (yfinance.ticker.Ticker): Ticker from yfinance
+        strikes_selected (list): [Optional] List of strikes to filter for from the original data
+        tenors_selected (list): [Optional] List of tenors (in 'YYYY-MM-DD' format) to filter for from the original data
+
+    Returns:
+        call_data (pd.DataFrame): DataFrame of Call data
+        put_data (pd.DataFrame):  DataFrame of Put data
+    """
+
+    all_tenors = ticker.options
+    if tenors_selected:
+        all_tenors = (x for x in all_tenors if x in tenors_selected)
+        print(f"Tenors reduced from {len(ticker.options)} to {len(all_tenors)}")
+
+    df = pd.DataFrame()
+    for x in all_tenors:
+        opt_tenor = ticker.option_chain(date=x)
+        # CALL
+        opt_calls = opt_tenor.calls
+        opt_calls.loc[:, 'put_call_code'] = 'C'
+
+        # PUT
+        opt_puts = opt_tenor.puts
+        opt_puts.loc[:, 'put_call_code'] = 'P'
+        tenor_data = pd.concat([opt_calls, opt_puts], ignore_index=True)
+        tenor_data.loc[:, 'Expiration'] = x
+        df = pd.concat([df, tenor_data], ignore_index=True)
+
+    if strikes_selected:
+        df = df.loc[df.strike.isin(strikes_selected), :]
+        print(f"Strikes selected :{strikes_selected}")
+
+    return df
