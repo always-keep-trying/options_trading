f2b564d4f8703f5e7cd080eaac88abe2098bc7b4
Luke Lee <luke.lee.python@gmail.com>
2025-06-23 15:49:32 -0500

    update folder directory name

--- README.md
+++ README.md
@@ -7,21 +7,21 @@ The aim of this repo is to generate signals for trading, construct strategies us
 ## Options Data
 
 Time series data of index and options data can be fetched from the [yfinance](https://ranaroussi.github.io/yfinance/index.html) python package.
-To simplify the process of fetching these data, the [support functions module](./code/support_functions.py) and a separate [module for visualizing](./code/plot_functions.py) were created.
+To simplify the process of fetching these data, the [support functions module](./src/support_functions.py) and a separate [module for visualizing](./src/plot_functions.py) were created.
 
-As an example I've fetched data related to "SPX" Index and their options in the [option data visualization notebook](./code/option_data_visualiztion.ipynb). The Implied volatility Skew/Smile has been plotted in the notebook.
+As an example I've fetched data related to "SPX" Index and their options in the [option data visualization notebook](./src/option_data_visualiztion.ipynb). The Implied volatility Skew/Smile has been plotted in the notebook.
 
 Few challenge that came up during exploration of the data source were...
 - The options data is the current day's data only. It does not give access to historical data. 
 - No greeks available (Delta, Vega, Rho, Gamma, and Theta).
-- Data quality issues. For example some implied volatility values seems to be set to a near zero value. In order to remove such observations the @clean_data decorator will be used to remove such observations. (see [support_functions.py](./code/support_functions.py)). 
+- Data quality issues. For example some implied volatility values seems to be set to a near zero value. In order to remove such observations the @clean_data decorator will be used to remove such observations. (see [support_functions.py](./src/support_functions.py)). 
 
 ## Signals
 
 The signals focused on this repository would be on relationship between volatility indices. Full disclosure here, this idea was introduced to me by Euan Sinclair from a webinar hosted by CBOE: "Finding Edge: A Key Part of Trading Process". During this webinar the relationship that was explored was between VXN(Cboe NASDAQ-100 Volatility Index) to VIX(30 day volatility of SPX). 
 
-However, expanding on this idea this repository takes it further by comparing other volatility indices (Volatility index of NDX, DJX, RUT, Gold, and Oil). To explore these relationships and dislocations is the [dislocation analysis](./code/dislocation_analysis.py) module was created.  
-**The results of these analysis and dislocations can be seen in [this notebook](./code/Finding_an_Edge.ipynb).**
+However, expanding on this idea this repository takes it further by comparing other volatility indices (Volatility index of NDX, DJX, RUT, Gold, and Oil). To explore these relationships and dislocations is the [dislocation analysis](./src/dislocation_analysis.py) module was created.  
+**The results of these analysis and dislocations can be seen in [this notebook](./src/Finding_an_Edge.ipynb).**
 
 *A new data source FRED has been added in this step. Some indices were not available in yfinance, but they were available in the [fredapi](https://pypi.org/project/fredapi/).*
 
@@ -29,7 +29,7 @@ However, expanding on this idea this repository takes it further by comparing ot
 
 Once the Signals indentify opportunities(i.e. dislocation in relationship) to invest, actions can be taken to profit by constructing a trade. For example a ATM Straddle of the indices (options on the SPX for VIX, options on the NDX for VXN etc...) would be a great option.
 
-In order to overcome the challenge of not having access to historical options data, we will collect daily options data. To accomplish this I've written the [Extract Transform and Load(ETL)](./code/ETL.py) module which can be run daily.
+In order to overcome the challenge of not having access to historical options data, we will collect daily options data. To accomplish this I've written the [Extract Transform and Load(ETL)](./src/ETL.py) module which can be run daily.
 
 - **More to come on this section. Specifically, the constuction of the straddles and their performances**
 
--- src/ETL.py
+++ src/ETL.py
@@ -4,7 +4,7 @@ import datetime
 import pandas as pd
 import yahoo.code.support_functions as sf
 import yahoo.code.dislocation_analysis as da
-
+import yahoo.code.ORATS_data as od
 
 class bcolors:
     HEADER = '\033[95m'
@@ -24,7 +24,7 @@ IS_VERBOSE = False
 class ETL:
     """Parent class for Extract Transform and Load process"""
 
-    def __init__(self, data_dir, ticker):
+    def __init__(self, data_dir: str, ticker: str | list):
         self.data = None
         self.data_dir = data_dir
         self.count = None
@@ -58,10 +58,13 @@ class ETL:
         raise NotImplementedError("To be implemented by child class")
 
     def run(self):
-        if os.path.exists(self.file_name):
+        if isinstance(self.ticker, str) and os.path.exists(self.file_name):
             if IS_VERBOSE:
                 print(f"File already exists: {self.file_name}")
             self.count = pd.read_parquet(self.file_name).shape[0]
+        elif isinstance(self.ticker, list):
+            [os.path.exists(self.file_name(x)) for x in self.ticker]
+
         elif self.today_dt.weekday() >= 5:  # check if it is a weekend
             print("today is a weekend and the ETL will not be executed")
         else:
@@ -75,8 +78,15 @@ class ETL:
                 print(f"{bcolors.WARNING}Warning: "
                       f"No data was extracted {self.ticker} during use of "
                       f"{self.__class__.__name__}{bcolors.ENDC}")
+        print(self)
 
-    def fetch(self):
+    def __str__(self):
+        p_str = f"{self.__class__.__name__} for ticker:{self.ticker} \n"
+        p_str += f"File: {self.file_name}\n"
+        p_str += f"Count: {self.count}"
+        return p_str
+
+    def fetch(self) -> pd.DataFrame:
         if os.path.exists(self.file_name):
             return pd.read_parquet(self.file_name)
         else:
@@ -86,7 +96,7 @@ class ETL:
 class UnderlyingETL(ETL):
     """Child class specific to Underlying ETL using the yfinance"""
 
-    def __init__(self, data_dir, ticker):
+    def __init__(self, data_dir: str, ticker: str):
         super().__init__(data_dir, ticker)
         underlying_dir = os.path.join(self.today_dir, 'Underlying')
         if not os.path.exists(underlying_dir):
@@ -114,12 +124,15 @@ class UnderlyingETL(ETL):
         assert df_size[0] > 0, "The file is empty!"
 
         assert {'Open', 'High', 'Low', 'Close'}.issubset(set(read_df.columns)), "there are missing columns (from OHLC)"
+        import pdb
+        pdb.set_trace()
+        # TODO: fix zero close value, read_df.iloc[-1,['Open', 'High', 'Low', 'Close']].to_list()
 
 
 class OptionETL(ETL):
     """Child class specific to Option ETL using the yfinance"""
 
-    def __init__(self, data_dir, ticker):
+    def __init__(self, data_dir: str, ticker: str):
         super().__init__(data_dir, ticker)
         option_dir = os.path.join(self.today_dir, 'Option')
         if not os.path.exists(option_dir):
@@ -148,32 +161,96 @@ class OptionETL(ETL):
             set(read_df.columns)), "there are missing columns (from OHLC)"
 
 
-def main(data_dir, index_opt_dict):
+class ORATS_Options(ETL):
+    """ get historical data """
+    def __init__(self, data_dir: str, ticker: list, trade_date: str):
+        self.trade_date = trade_date
+        super().__init__(data_dir, ticker)
+        self.today_dir = None
+
+    def create_dir(self):
+
+        option_dir = os.path.join(self.data_dir, self.trade_date, 'Option')
+        if not os.path.exists(option_dir):
+            os.makedirs(option_dir)
+            print("Created new folder: " + option_dir)
+        self.file_name = lambda x: os.path.join(option_dir, "Option" + x + '.parquet')
+
+    def extract(self):
+        self.data = od.get_hist_strikes(tickers=self.ticker, trade_date=self.trade_date)
+
+    def transform(self):
+        # subset data by ticker
+        all_tickers = self.data['ticker'].drop_duplicates().to_list()
+        data_dict = {}
+        for symb in all_tickers:
+            data_dict[symb] = self.data.loc[self.data['ticker'] == symb, :].reset_index(drop=False).copy()
+        # convert the pd.DataFrame to dict
+        self.data = data_dict
+
+    def load(self):
+        self.count = 0
+        for key, value in self.data.items():
+            value.to_parquet(self.file_name(key))
+            self.count += value.shape[0]
+
+    def validate(self):
+        # very simple validation
+        for key, value in self.data.items():
+            read_df = pd.read_parquet(self.file_name(key))
+            df_size = read_df.shape
+            assert df_size[0] > 0, f"The file for {key} is empty!"
+
+            assert {'ticker', 'tradeDate', 'expirDate', 'dte', 'strike', 'stockPrice'}.issubset(
+                set(read_df.columns)), "Contract info is missing!"
+
+            assert {'delta', 'gamma', 'theta', 'vega', 'rho'}.issubset(
+                set(read_df.columns)), "Greeks are missing!"
+
+
+def main(data_dir: str, index_opt_dict: dict):
     """ main function to run the ETL"""
-    ETL_summary = pd.DataFrame()
-    today_dir = None
-    for index_sym, opt_sym in index_opt_dict.items():
-        und = UnderlyingETL(data_dir=data_dir, ticker=index_sym)
-        und.run()
+    now_dt = datetime.datetime.now()
+    market_close = datetime.time(12 + 3, 15)  # 3:15 pm CST
+    if now_dt.time() < market_close:
+        print("Market is still open, skipping ETL from yfinance")
+    else:
+        ETL_summary = pd.DataFrame()
+        today_dir = None
+        for index_sym, opt_sym in index_opt_dict.items():
+            und = UnderlyingETL(data_dir=data_dir, ticker=index_sym)
+            und.run()
 
-        opt = OptionETL(data_dir=data_dir, ticker=opt_sym)
-        opt.run()
+            if isinstance(opt_sym, str):
+                opt_sym = [opt_sym]
 
-        summary = pd.DataFrame(
-            {'Vol_index': index_sym,
-             'Vol_index_data': und.count,
-             'Option': opt_sym,
-             'Option_data': opt.count
-             }, index=[0]
+            for x in opt_sym:
+                opt = OptionETL(data_dir=data_dir, ticker=x)
+                opt.run()
+
+                summary = pd.DataFrame(
+                    {'Vol_index': index_sym,
+                     'Vol_index_data': und.count,
+                     'Option': x,
+                     'Option_data': opt.count
+                     }, index=[0]
+                )
+                ETL_summary = pd.concat([ETL_summary, summary], ignore_index=True)
+
+                today_dir = today_dir or und.today_dir or opt.today_dir
+        print(ETL_summary)
+        ETL_summary.to_csv(
+            os.path.join(today_dir, "ET_Summary_" + os.path.basename(today_dir) + ".csv"),
+            index=False
         )
-        ETL_summary = pd.concat([ETL_summary, summary], ignore_index=True)
 
-        today_dir = today_dir or und.today_dir or opt.today_dir
-    print(ETL_summary)
-    ETL_summary.to_csv(
-        os.path.join(today_dir, "ET_Summary_" + os.path.basename(today_dir) + ".csv"),
-        index=False
-    )
+
+def main_ORATS_hist(data_dir: str, tickers: list, ref_date: str):
+    """ Fetch historical data of options using ORATS API"""
+
+    opt_hist = ORATS_Options(data_dir=data_dir, ticker=tickers, trade_date=ref_date)
+    opt_hist.run()
+    print(f"ETL completed for symbols:{tickers}, total count of data: {opt_hist.count}")
 
 
 def fetch_data(ticker: str, is_option: bool = True,
@@ -207,7 +284,7 @@ if __name__ == "__main__":
         "^VIX": "^VIX",  # replication/examples, VIX
         "^VXN": "^NDX",  # replication, Nasdaq-100
         "^VXD": "^DJX",  # example 1, Dow Jones Industrial Average
-        "^OVX": "UCO",  # example 2, Crude Oil
+        "^OVX": ["UCO", "BNO"],  # example 2, Crude Oil
         "^RVX": "^RUT",  # example 3, Russell-2000
         "^GVZ": "GLD"  # example 4 and 5, Gold
     }
@@ -216,3 +293,5 @@ if __name__ == "__main__":
         print("skipping ETL during weekend")
     else:
         main(data_dir, index_opt_dict)
+
+    main_ORATS_hist(data_dir=data_dir, tickers=["SPX", "BNO", "UCO"], ref_date='2020-04-21')
