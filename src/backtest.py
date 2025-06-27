046b15953b346643fcb4a37009dafc5478b04533
Luke Lee <luke.lee.python@gmail.com>
2025-06-27 18:05:39 -0500

    updates to the backtesting module

--- src/backtest.py
+++ src/backtest.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 import yahoo.src.ORATS_data as od
 
@@ -67,16 +68,135 @@ def extract_positions(position_df: pd.DataFrame) -> None:
         position_df: Position DataFrame
 
     Returns:
-
+        datafroame of all contracts history
     """
     posn_df = position_df.loc[:, ['ticker', 'expirDate', 'strike']].drop_duplicates()
     posn_list = posn_df.values.tolist()
 
     for one_posn in posn_list:
         extract_contract_data(*one_posn)
-    print("Positions processed")
-    print(posn_df)
 
+    print(posn_df)
+    print("Positions processed")
+
+def calculat_payoff(df, is_call=True):
+    if is_call:
+        # call payoff
+        payoff = list(map(lambda x: np.max([x,0]), df['stockPrice'].to_numpy()-df['strike'].to_numpy()))
+    else:
+        # put payoff
+        payoff = list(map(lambda x: np.max([x,0]), df['strike'].to_numpy()-df['stockPrice'].to_numpy()))
+    return payoff
+
+
+class Backtest:
+    def __init__(self, positon_df):
+        self.position_df = positon_df
+        assert 'Netquantity' in self.position_df.columns, 'Please define Netquantity column'
+        # one business date
+        self.trade_date = self.position_df['tradeDate'].drop_duplicates().squeeze()
+
+        self.symbols_list = self.position_df['ticker'].drop_duplicates().to_list()
+        self.contract_hist = {}
+        self.get_contract_hist()
+
+        self.portfolio_payoff = None
+        self.portfolio_value = None
+        self.underlying_price_hist = None
+
+
+    def get_contract_hist(self):
+        # run ETL through all position
+        extract_positions(self.position_df)
+
+        for symb in self.symbols_list:
+
+            symb_posn_df = self.position_df.query(f"ticker == '{symb}'").drop(columns='put_call_code')
+            symb_posn_df = symb_posn_df.loc[:,['ticker', 'tradeDate', 'expirDate', 'dte', 'strike', 'stockPrice']].drop_duplicates()
+            assert symb_posn_df.shape[0] == 1, 'Processing one contract at a time'
+            expiration, strike = symb_posn_df[['expirDate', 'strike']].squeeze().values
+            contract_hist = extract_contract_data(ticker=symb, exp_dt=expiration, strike_val=strike)
+            # each contract_hist will contain both put and call data
+            self.contract_hist[symb] = contract_hist.loc[contract_hist.tradeDate>=self.trade_date,:].reset_index(drop=True)
+
+
+    def backtest(self):
+        portfolio_payoff = pd.DataFrame()
+        portfolio_value = pd.DataFrame()
+
+        underlying_price_hist = pd.DataFrame()
+        for symb in self.symbols_list:
+            contract_hist = self.contract_hist[symb]
+            for _, row in self.position_df.query(f"ticker == '{symb}'").iterrows():
+                # looking forward, does not include the date the trade was entered
+                curr_posn = contract_hist.loc[contract_hist.tradeDate > self.trade_date, :].copy()
+                curr_posn = curr_posn.merge(
+                    row.to_frame().T.loc[:, ['ticker', 'expirDate', 'strike', 'put_call_code', 'Netquantity']])
+                curr_posn = curr_posn.sort_values('dte', ascending=False).reset_index(drop=True)
+                curr_posn.loc[:, 'Payoff'] = calculat_payoff(curr_posn, is_call=(row['put_call_code'] == 'C'))
+                curr_posn.loc[:, 'Net_Payoff'] = curr_posn['Payoff'] * np.float64(curr_posn.Netquantity)
+                curr_posn.loc[:, 'Net_Value'] = curr_posn['Value'] * np.float64(curr_posn.Netquantity)
+                #curr_posn = curr_posn.set_index('tradeDate')
+                contract_detail = "_".join(map(str, row[['ticker', 'expirDate', 'put_call_code']].values)) + '_' + str(
+                    row['strike']).replace('.0', '').replace('.', '-')
+                print(contract_detail)
+
+                if portfolio_payoff.empty:
+                    portfolio_payoff = curr_posn.loc[:, ['tradeDate','Net_Payoff']].rename(columns={'Net_Payoff': contract_detail})
+                    portfolio_value = curr_posn.loc[:, ['tradeDate','Net_Value']].rename(columns={'Net_Value': contract_detail})
+                else:
+                    # import pdb
+                    # pdb.set_trace()
+
+                    portfolio_payoff = portfolio_payoff.merge(
+                        curr_posn.loc[:, ['tradeDate','Net_Payoff']].rename(columns={'Net_Payoff': contract_detail}),
+                        how='inner', on='tradeDate')
+                    portfolio_value = portfolio_value.merge(
+                        curr_posn.loc[:, ['tradeDate','Net_Value']].rename(columns={'Net_Value': contract_detail}),
+                        how='inner')
+
+
+            if underlying_price_hist.empty:
+                underlying_price_hist = contract_hist.loc[contract_hist.put_call_code == 'C',['tradeDate','stockPrice']].rename(columns={'stockPrice':symb})
+            else:
+                underlying_price_hist = underlying_price_hist.merge(
+                    contract_hist.loc[contract_hist.put_call_code == 'C',['tradeDate','stockPrice']].rename(columns={'stockPrice':symb}),
+                    how='outer', on='tradeDate'
+                )
+
+
+        portfolio_payoff = portfolio_payoff.set_index('tradeDate')
+        portfolio_value = portfolio_value.set_index('tradeDate')
+        underlying_price_hist = underlying_price_hist.set_index('tradeDate')
+
+        portfolio_payoff.loc[:, 'Payoff'] = portfolio_payoff.values.sum(axis=1)
+        portfolio_value.loc[:, 'Value'] = portfolio_value.values.sum(axis=1)
+
+        portfolio_payoff = portfolio_payoff.merge(underlying_price_hist, how='outer', left_index=True, right_index=True)
+        portfolio_value = portfolio_value.merge(underlying_price_hist, how='outer',  left_index=True, right_index=True)
+
+        portfolio_payoff.index = pd.to_datetime(portfolio_payoff.index)
+        portfolio_value.index = pd.to_datetime(portfolio_value.index)
+        underlying_price_hist.index = pd.to_datetime(underlying_price_hist.index)
+
+
+        for symb in self.symbols_list:
+            portfolio_payoff.loc[:, symb+'_Payoff'] = \
+                portfolio_payoff.loc[:, [x for x in portfolio_payoff.columns if x.startswith(symb+'_')]].values.sum(1)
+            portfolio_value.loc[:, symb + '_Value'] = \
+                portfolio_value.loc[:, [x for x in portfolio_value.columns if x.startswith(symb + '_')]].values.sum(1)
+
+        self.portfolio_payoff = portfolio_payoff
+        self.portfolio_value = portfolio_value
+        self.underlying_price_hist = underlying_price_hist
+
+
+    def calculate_time_series(self, symbol, column_name):
+
+        contract_hist = self.contract_hist[symbol]
+        contract_hist.loc[:,'Calculate'] = contract_hist.loc[:,column_name]*contract_hist.loc[:, 'Netquantity']
+
+        return contract_hist.groupby(['tradeDate'])['Calculate'].sum().rename(column={'Calculate':'Agg_'+column_name})
 
 
 
